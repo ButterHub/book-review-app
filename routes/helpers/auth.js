fe7cc142ac6855af025f936d87bb7240fe27c889
Adrian Butterworth <24711048+ButterHub@users.noreply.github.com>
2019-01-12 13:28:09 +0000

    Added delete with login only

--- routes/helpers/auth.js
+++ routes/helpers/auth.js
@@ -5,7 +5,7 @@ module.exports = {
     } else {
       req.flash(
         "error_msg",
-        "You were not authorised to view that private page."
+        "You must be logged in and have permission for that action."
       );
       res.redirect("/users/login");
       // Now need to figure out protected routes
